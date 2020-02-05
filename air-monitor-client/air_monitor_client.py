8aa1551950c3f9dc0a0e3c7e66a9f1afe1ae2b08
gbaman <gbaman@users.noreply.github.com>
2020-02-05 19:52:42 +0000

    Switch to storing the node_key in a config.py file

--- air-monitor-client/air_monitor_client.py
+++ air-monitor-client/air_monitor_client.py
@@ -2,6 +2,8 @@ from CO2Meter import *
 import time
 import requests
 
+from config import node_key
+
 base_url = "http://10.0.12.70:5000"
 DELAY_TIME = 120
 
@@ -12,11 +14,11 @@ while True:
     data = sensor.get_data()
     try:
         if 'co2' in data:
-            requests.get(f"{base_url}/upload_data_live/ac9d8f74-d8b0-40a1-a1a1-0587d7aac624/co2/{data['co2']}")
+            requests.get(f"{base_url}/upload_data_live/{node_key}/co2/{data['co2']}")
             print(f"Data sent to server for co2 of {data['co2']}")
             time.sleep(0.2)
         if 'temperature' in data:
-            requests.get(f"{base_url}/upload_data_live/ac9d8f74-d8b0-40a1-a1a1-0587d7aac624/temperature/{data['temperature']}")
+            requests.get(f"{base_url}/upload_data_live/{node_key}/temperature/{data['temperature']}")
             print(f"Data sent to server for temperature of {data['temperature']}")
     except requests.RequestException as e:
         print(f"Unable to connect, waiting for {DELAY_TIME} seconds...")
